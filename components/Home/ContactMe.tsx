3aa8bf91463d6b1168f5a42ec1410c0fb6933285
junaisbabu <junaisdvs@gmail.com>
2023-09-26 02:01:34 +0530

    Integrate emailjs

--- components/Home/ContactMe.tsx
+++ components/Home/ContactMe.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import {
   Card,
   CardContent,
@@ -10,8 +12,40 @@ import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 import { Button } from "../ui/button";
 import { contact } from "../icons/svg";
+import emailjs from "@emailjs/browser";
+import { useToast } from "../ui/toast/use-toast";
 
 function ContactMe() {
+  const { toast } = useToast();
+  const [isSending, setIsSending] = useState(false);
+
+  const sendEmail = async (e: any) => {
+    e.preventDefault();
+    setIsSending(true);
+    const response = await emailjs.sendForm(
+      process.env.NEXT_PUBLIC_SERVICE_ID || "",
+      process.env.NEXT_PUBLIC_TEMPLATE_ID || "",
+      e.target,
+      process.env.NEXT_PUBLIC_PUBLIC_KEY || ""
+    );
+
+    if (response.status === 200) {
+      toast({
+        status: "success",
+        title: "Success",
+        description: "Successfully Submitted",
+      });
+      e.target.reset();
+      setIsSending(false);
+    } else {
+      toast({
+        status: "error",
+        title: "Error",
+        description: "Something went wrong",
+      });
+    }
+  };
+
   return (
     <Card className="h-fit rounded-2xl">
       <CardHeader>
@@ -26,23 +60,31 @@ function ContactMe() {
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <form>
+        <form onSubmit={sendEmail}>
           <div className="space-y-4">
             <Input
               type="text"
               placeholder="Name"
               className="bg-transparent rounded-md"
+              name="from_name"
+              required
             />
             <Input
               type="email"
               placeholder="Email"
               className="bg-transparent rounded-md"
+              name="from_email"
+              required
             />
             <Textarea
               placeholder="Message..."
               className="bg-transparent rounded-md resize-none"
+              name="message"
+              required
             />
-            <Button className="w-full">Send Message</Button>
+            <Button className="w-full" type="submit">
+              {isSending ? "Sending..." : "Send Message"}
+            </Button>
           </div>
         </form>
       </CardContent>
